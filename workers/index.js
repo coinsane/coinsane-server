2cc4befcf3b3b3b03bce184970ff42bed18f842f
Artem Kashin <dev.tema@gmail.com>
2018-02-18 00:30:42 +0300

    start workers

--- workers/index.js
+++ workers/index.js
@@ -3,6 +3,10 @@ const fetchPrices = require('./fetchPrices');
 const fetchCoins = require('./fetchCoins');
 
 function startWorkers() {
+  setTimeout(() => updateTotals(), 30 * 1000);
+  setTimeout(() => fetchPrices(), 60 * 1000);
+  fetchCoins();
+
   setInterval(() => updateTotals(), 60 * 1000); // every 1 min
   setTimeout(() => setInterval(() => fetchPrices(), 60 * 1000), 30 * 1000); // every 1 min, starts after 30 sec wait
   setInterval(() => fetchCoins(), 24 * 60 * 60 * 1000); // every day
