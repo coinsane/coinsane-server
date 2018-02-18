c931a2ced9e0cb5f2dfe425799328357e09082ae
Artem Kashin <dev.tema@gmail.com>
2018-02-18 23:38:44 +0300

    fix

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const { startWorkers } = require('./workers');
 
 mongoose.connect(config.mongo.uri, config.mongo.options, (err, res) => {
   if (err) {
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    console.log ('ERROR connecting to: ' + config.mongo.uri + '. ' + err);
   } else {
-    console.log ('Succeeded connected to: ' + uristring);
+    console.log ('Succeeded connected to: ' + config.mongo.uri);
   }
 });
 mongoose.Promise = Promise;
