95dea4d8c14d41adfbf294e98cb96aa2b59b899a
Artem Kashin <dev.tema@gmail.com>
2018-03-06 00:54:25 +0300

    coin image in portfolio

--- server/api/portfolios.js
+++ server/api/portfolios.js
@@ -47,7 +47,7 @@ function getPortfolios(req, res, next) {
            {
              path: 'market',
              model: 'Market',
-             select: 'name symbol prices.BTC.price prices.USD.price prices.RUB.price',
+             select: 'name symbol imageUrl prices.BTC.price prices.USD.price prices.RUB.price',
            },
          ],
        },
